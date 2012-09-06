abded86d1515a5617d8eb4da09850ea69f2b1e3f
Ivan Jašo <jasojim@gmail.com>
2012-09-06 20:31:38 +0200

    Callback and other functions Implemented a callback function and moved the rule tests to separate functions.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,15 @@
 function validate(formName) {
-	var v = new Validator(formName);
+	var v = new Validator(formName, function(errs) {
+		var err_out = document.getElementById("ispis");
+		for (i = 0; i < errs.length; i++) {
+			var rule = errs[i];
+			err_out.innerHTML = err_out.innerHTML + rule.label + ": " + rule.rule + "<br>";
+		}
+	});
 	v.setRules("firstname", "First Name", "trim|required|min_length[5]|max_length[12]");
 	v.setRules("lastname", "Last Name", "trim|required|min_length[5]|max_length[12]");
 	v.setRules("password", "Password", "trim|required|is_natural");
-	v.setRules("passconf", "Password Confirmation", "trim|encode_php_tags");
+	v.setRules("passconf", "Password Confirmation", "trim|encode_php_tags|matches[password]");
 	v.setRules("email", "Email", "trim|required|valid_email");
 	return v.run();
 }
--- ttcformvalid.js
+++ ttcformvalid.js
@@ -1,17 +1,193 @@
-function Validator(form_name) {
-	//	TODO: Add callback function
+function Validator(form_name, callback) {
 	var form = document.forms[form_name];
 	
 	/*
 		Trim is separated from all the other rules
 		to avoid mistakes while testing those other rules
 	*/
+	//	TODO: rename trim_array to prepping_array
 	var trim_array = new Array();
 	var rules_array = new Array();
 	var err_array = new Array();
+	var passed = true;
+	
+	//-----------------------------
+	//	Internal functions
+	//-----------------------------
+	
+	function failed(rule) {
+		err_array.push(rule);
+		passed = false;
+	}
+	
+	/*	TODO: Create check_email so it can be used by both
+	*	the valid_email function and the valid_emails function
+	*/
+	
+	//-----------------------------
+	//	Prepping functions
+	//-----------------------------
 	
 	function trim(val) {
 		return val.replace(/^\s+|\s+$/g,"");
+	}
+	
+	function xss_clean() {
+		
+	}
+	
+	function prep_for_form() {
+		
+	}
+	
+	function prep_url() {
+		
+	}
+	
+	function strip_image_tags() {
+		
+	}
+	
+	function encode_php_tags(rule) {
+		form[rule.field].value = form[rule.field].value.replace(/</, "&lt;");
+		form[rule.field].value = form[rule.field].value.replace(/>/, "&gt;");
+	}
+	
+	//-----------------------------
+	//	Rule checking functions
+	//-----------------------------
+	
+	function required(rule) {
+		if (form[rule.field].value == "" || form[rule.field].value == null) {
+			failed(rule);
+		}
+	}
+	
+	function matches(rule) {
+		var who = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
+		if (form[rule.field].value != form[who].value) {
+			failed(rule);
+		}
+	}
+	
+	function is_unique(rule) {
+		
+	}
+	
+	function min_length(rule) {
+		var length = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
+		if (form[rule.field].value.length < length) {
+			failed(rule);
+		}
+	}
+	
+	function max_length(rule) {
+		var length = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
+		if (form[rule.field].value.length > length) {
+			failed(rule);
+		}
+	}
+	
+	function exact_length(rule) {
+		var length = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
+		if (form[rule.field].value.length != length) {
+			failed(rule);
+		}
+	}
+	
+	function greater_than(rule) {
+		var value = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
+		if (form[rule.field].value < value) {
+			failed(rule);
+		}
+	}
+	
+	function less_than(rule) {
+		var value = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
+		if (form[rule.field].value > value) {
+			failed(rule);
+		}
+	}
+	
+	function alpha(rule) {
+		var pattern = /^[a-z]+$/i;
+		if (!pattern.test(form[rule.field].value)) {
+			failed(rule);
+		}
+	}
+	
+	function alpha_numeric(rule) {
+		var pattern = /^([a-z0-9])+$/i;
+		if (!pattern.test(form[rule.field].value)) {
+			failed(rule);
+		}
+	}
+	
+	function alpha_dash(rule) {
+		var pattern = /^([-a-z0-9_-])+$/i;
+		if (!pattern.test(form[rule.field].value)) {
+			failed(rule);
+		}
+	}
+	
+	function numeric(rule) {
+		var pattern = /^[\-+]?[0-9]*\.?[0-9]+$/;
+		if (!pattern.test(form[rule.field].value)) {
+			failed(rule);
+		}
+	}
+	
+	function integer(rule) {
+		var pattern = /^[\-+]?[0-9]+$/;
+		if (!pattern.test(form[rule.field].value)) {
+			failed(rule);
+		}
+	}
+	
+	function decimal(rule) {
+		var pattern = /^[\-+]?[0-9]+\.[0-9]+$/;
+		if (!pattern.test(form[rule.field].value)) {
+			failed(rule);
+		}
+	}
+	
+	function is_natural(rule) {
+		var pattern = /^[0-9]+$/;
+		if (!pattern.test(form[rule.field].value)) {
+			failed(rule);
+		}
+	}
+	
+	function is_natural_no_zero(rule) {
+		var pattern = /^[0-9]+$/;
+		if (!pattern.test(form[rule.field].value) || form[rule.field].value == 0) {
+			failed(rule);
+		}
+	}
+	
+	function valid_email(rule) {
+		var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z.]{2,4}$/;
+		if (!pattern.test(form[rule.field].value)) {
+			failed(rule);
+		}
+	}
+	
+	function valid_emails(rule) {
+		//	TODO: separate emails (comma separated) and check them
+	}
+	
+	function valid_ip(rule) {
+		var pattern = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+		if (!pattern.test(form[rule.field].value)) {
+			failed(rule);
+		}
+	}
+	
+	function valid_base64(rule) {
+		var pattern = /[^a-zA-Z0-9\/\+=]/;
+		if (!pattern.test(form[rule.field].value)) {
+			failed(rule);
+		}
 	}
 	
 	this.setRules = function(field, label, rules) {
@@ -20,6 +196,7 @@ function Validator(form_name) {
 		
 		rules = rules.split("|");
 		for (var i = 0; i < rules.length; i++) {
+			//	TODO: check if rule belongs to prepping array and push it there
 			//	Check if the rule is "trim"
 			if (rules[i] == "trim") {
 				trim_array.push({"field":field});
@@ -30,7 +207,8 @@ function Validator(form_name) {
 	};
 	
 	this.run = function() {
-		var passed = true;
+		passed = true;
+		//	TODO: First do all of the prepping functions
 		//	First do all of the trimming
 		for (var i = 0; i < trim_array.length; i++) {
 			var polje = form[trim_array[i].field];
@@ -39,139 +217,59 @@ function Validator(form_name) {
 		
 		document.getElementById('ispis').innerHTML = "";
 		for (var i = 0; i < rules_array.length; i++) {
-			//	TODO: implement missing CI rules!!!
-			//	TODO: maybe move all of these to separate functions?
 			var rule = rules_array[i];
 			var polje = form[rule.field];
+			
 			if (rule.rule == "required") {
-				if (form[rule.field].value == "" || form[rule.field].value == null) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				required(rule);
+			} else if (rule.rule == "is_unique") {
+				//	TODO: is_unique
 			} else if (rule.rule == "valid_email") {
-				pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z.]{2,4}$/;
-				if (!pattern.test(form[rule.field].value)) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				valid_email(rule);
+			} else if (rule.rule == "valid_emails") {
+				valid_emails(rule);
 			} else if (rule.rule == "valid_ip") {
-				pattern = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
-				if (!pattern.test(form[rule.field].value)) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				valid_ip(rule);
 			} else if (rule.rule == "alpha") {
-				pattern = /^[a-z]+$/i;
-				if (!pattern.test(form[rule.field].value)) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				alpha(rule);
 			} else if (rule.rule == "alpha_numeric") {
-				pattern = /^([a-z0-9])+$/i;
-				if (!pattern.test(form[rule.field].value)) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				alpha_numeric(rule);
 			} else if (rule.rule == "alpha_dash") {
-				pattern = /^([-a-z0-9_-])+$/i;
-				if (!pattern.test(form[rule.field].value)) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				alpha_dash(rule);
 			} else if (rule.rule == "numeric") {
-				pattern = /^[\-+]?[0-9]*\.?[0-9]+$/;
-				if (!pattern.test(form[rule.field].value)) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				numeric(rule);
 			} else if (rule.rule == "integer") {
-				pattern = /^[\-+]?[0-9]+$/;
-				if (!pattern.test(form[rule.field].value)) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				integer(rule);
 			} else if (rule.rule == "decimal") {
-				pattern = /^[\-+]?[0-9]+\.[0-9]+$/;
-				if (!pattern.test(form[rule.field].value)) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				decimal(rule);
 			} else if (rule.rule == "is_natural") {
-				pattern = /^[0-9]+$/;
-				if (!pattern.test(form[rule.field].value)) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				is_natural(rule);
 			} else if (rule.rule == "is_natural_no_zero") {
-				pattern = /^[0-9]+$/;
-				if (!pattern.test(form[rule.field].value) || form[rule.field].value == 0) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				is_natural_no_zero(rule);
 			} else if (rule.rule == "valid_base_64") {
-				pattern = /[^a-zA-Z0-9\/\+=]/;
-				if (!pattern.test(form[rule.field].value)) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				valid_base64(rule);
 			} else if (rule.rule == "encode_php_tags") {
-				form[rule.field].value = form[rule.field].value.replace(/</, "&lt;");
-				form[rule.field].value = form[rule.field].value.replace(/>/, "&gt;");
-			} else if (/match/g.test(rule.rule)) {
-				var who = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
-				if (form[rule.field].value != form[who].value) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				//	TODO: move this to the front like trim
+				encode_php_tags(rule);
+			} else if (/matches/g.test(rule.rule)) {
+				matches(rule);
 			} else if (/min_length/g.test(rule.rule)) {
-				var length = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
-				if (form[rule.field].value.length <= length) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				min_length(rule);
 			} else if (/max_length/g.test(rule.rule)) {
-				var length = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
-				if (form[rule.field].value.length >= length) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				max_length(rule);
 			} else if (/exact_length/g.test(rule.rule)) {
-				var length = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
-				if (form[rule.field].value.length != length) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				exact_length(rule);
 			} else if (/greater_than/g.test(rule.rule)) {
-				var value = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
-				if (form[rule.field].value < value) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				greater_than(rule);
 			} else if (/less_than/g.test(rule.rule)) {
-				var value = rule.rule.substr(rule.rule.indexOf("[") + 1, rule.rule.indexOf("]") - rule.rule.indexOf("[") - 1);
-				if (form[rule.field].value > value) {
-					document.getElementById('ispis').innerHTML = document.getElementById('ispis').innerHTML + rule.label + ": " + rule.rule + "<br>";
-					err_array.push(rule);
-					passed = false;
-				}
+				less_than(rule);
 			}
+		}
+		
+		//	If the callback function exists
+		if (callback instanceof Function) {
+			//	Run it with error array as an argument
+			callback(err_array);
 		}
 		return passed;
 	};
